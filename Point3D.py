1b8a31b1dcc489e64352b79443d85ca93c97199a
Brahma Teja Ponnuru <teja463@users.noreply.github.com>
2017-12-24 00:10:22 +0530

    Added few more examples
    for python programs

---
+++ Point3D.py
@@ -0,0 +1,10 @@
+class Point3D(object):
+    def hello(self):
+        print ("Hello")
+    
+#  def __repr__():
+ #   return "(%d, %d, %d)" % (self.x, self.y, self.z)
+  
+  my_point = Point3D()
+  my_point.hello()
+  print (my_point)
